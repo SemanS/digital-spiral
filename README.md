e494446181cb84a2e86c101a50beb3b21676e503
Slavo Seman <slavomir.seman@hotovo.com>
2025-09-17 18:07:10 +0200

    refactor

--- README.md
+++
@@ -1,1 +0,0 @@
-# digital-spiral
--- digital_spiral/__init__.py
+++
@@ -1,8 +0,0 @@
-"""digital_spiral package.
-
-This package provides tools for building spiral attention masks.
-"""
-
-from .spiral_mask import build_spiral_mask
-
-__all__ = ["build_spiral_mask"]
